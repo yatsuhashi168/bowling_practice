49833190102a96121eb13ae9d61ccb51a05ab059
yatsuhashi168 <kurimi777luk@gmail.com>
2021-06-30 13:28:57 +0900

    スコアを二つずつ配列に分割

--- bowling_practice.rb
+++ bowling_practice.rb
@@ -8,10 +8,19 @@ shots = []
 scores.each do |n|
   if n != 'X'
     shots << n.to_i
+  elsif n == 'X' && shots.size.odd?
+    shots << 10
   else
     shots << 10
     shots << 0 if shots.size < 18
   end
 end
-
 p shots
+
+frames = []
+shots.first(18).each_slice(2) do |n|
+  frames << n
+end
+frames << shots.slice(18..20)
+
+p frames
