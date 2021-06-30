3c136919a906334aad32f8d4f0e887a04602241f
yatsuhashi168 <kurimi777luk@gmail.com>
2021-06-30 14:40:49 +0900

    スコアの計算部分を追加、プロトタイプ完成

--- bowling_practice.rb
+++ bowling_practice.rb
@@ -2,7 +2,6 @@
 
 score = ARGV[0]
 scores = score.split(',')
-p scores
 
 shots = []
 scores.each do |n|
@@ -15,7 +14,6 @@ scores.each do |n|
     shots << 0 if shots.size < 18
   end
 end
-p shots
 
 frames = []
 shots.first(18).each_slice(2) do |n|
@@ -23,4 +21,20 @@ shots.first(18).each_slice(2) do |n|
 end
 frames << shots.slice(18..20)
 
-p frames
+point = 0
+frames.each.with_index do |frame, i|
+  if frame[0] == 10 && frame.size == 2
+    point += frames[i + 1][0]
+    point +=
+      if frames[i + 1][0] == 10 && frames[i + 1].size == 2
+        frames[i + 2][0]
+      else
+        frames[i + 1][1]
+      end
+  elsif frame.sum == 10 && frame.size == 2
+    point += frames[i + 1][0]
+  end
+  point += frame.sum
+end
+
+puts point
